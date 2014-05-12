633279fe037c7568646196dbf13bd3f997d29a0c
Christiaan van Bergen <christiaan@nonerds.nl>
2014-05-12 15:58:03 +0200

    Add of JsonSerializer to LayoutRenderers
    Add of JsonSerializer to LayoutRenderers to projects NLog.netfx40 and
    NLog.netfx45

---
+++ src/NLog/LayoutRenderers/SerializeJsonLayoutRenderer.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization.Json;
+using System.Text;
+using NLog.Config;
+
+namespace NLog.LayoutRenderers
+{
+    /// <summary>
+    /// The serialized object (message) in JSON
+    /// </summary>
+    [LayoutRenderer("jsonserialized")]
+    [ThreadAgnostic]
+    public class SerializeJsonLayoutRenderer : LayoutRenderer
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="builder"></param>
+        /// <param name="logEvent"></param>
+        protected override void Append(StringBuilder builder, LogEventInfo logEvent)
+        {
+            try
+            {
+                logEvent.Parameters.ToList().ForEach(n =>
+                {
+                    if (n.GetType().IsClass) return;
+                    var serializerObj = new DataContractJsonSerializer(n.GetType());
+                    using (var ms = new MemoryStream())
+                    {
+                        serializerObj.WriteObject(ms, n);
+                        ms.Seek(0, SeekOrigin.Begin);
+                        using (var sr = new StreamReader(ms))
+                        {
+                            builder.Append(sr.ReadToEnd());
+                        }
+                    }
+                });
+
+            }
+            catch (Exception)
+            {
+                builder.Append("Failed to serialize object:");
+                builder.Append(logEvent.FormattedMessage);
+            }
+        }
+    }
+}
