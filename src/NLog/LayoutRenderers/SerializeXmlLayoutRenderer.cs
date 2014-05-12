7dc8ecc08213622cfd79180663d72e6497018f1a
Christiaan van Bergen <christiaan@nonerds.nl>
2014-05-12 15:53:28 +0200

    Add of XmlSerializer to LayoutRenderers
    Add of XmlSerializer to LayoutRenderers
    Add of XmlHelper to projects for NLog.netfx40 and NLog.netfx35

---
+++ src/NLog/LayoutRenderers/SerializeXmlLayoutRenderer.cs
@@ -0,0 +1,52 @@
+﻿#if !SILVERLIGHT
+
+namespace NLog.LayoutRenderers
+{
+    using System;
+    using System.IO;
+    using System.Linq;
+    using System.Text;
+    using System.Xml.Serialization;
+    using NLog.Config;
+
+    /// <summary>
+    /// The xml-serialized object (message)
+    /// </summary>
+    [LayoutRenderer("xmlserialized")]
+    [ThreadAgnostic]
+    public class SerializeXmlLayoutRenderer : LayoutRenderer
+    {
+        /// <summary>
+        /// Renders the specified object as xml via XML-serialization and appends it to the specified <see cref="StringBuilder" />.
+        /// </summary>
+        /// <param name="builder"></param>
+        /// <param name="logEvent"></param>
+        protected override void Append(StringBuilder builder, LogEventInfo logEvent)
+        {
+            try
+            {
+                logEvent.Parameters.ToList().ForEach(n =>
+                {
+                    if (n.GetType().IsClass) return;
+                    var serializerObj = new XmlSerializer(n.GetType());
+                    using (var ms = new MemoryStream())
+                    {
+                        serializerObj.Serialize(ms, n);
+                        ms.Seek(0, SeekOrigin.Begin);
+                        using (var sr = new StreamReader(ms))
+                        {
+                            builder.Append(sr.ReadToEnd());
+                        }
+                    }
+                });
+
+            }
+            catch (Exception)
+            {
+                builder.Append("Failed to serialize object:");
+                builder.Append(logEvent.FormattedMessage);
+            }
+        }
+    }
+}
+#endif 
